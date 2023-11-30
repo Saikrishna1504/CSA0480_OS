e560899317fae2e050b2addfa6b038e2acf50af9
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2023-11-30 22:10:57 +0530

    Create lool_Clook

---
+++ lool_Clook.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("enter the number of requests: ");
+	scanf("%d",&n);
+	int req[n];
+	printf("enter the requests: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&req[i]);
+	}
+	int head;
+	printf("enter the initial head position: ");
+	scanf("%d",&head);
+    int end;
+    for(i=0;i<n-1;i++)
+    {
+    	for(j=0;j<n-i-1;j++)
+    	{
+    		if(req[j]>req[j+1])
+    		{
+    			int temp = req[j];
+    			req[j]=req[j+1];
+    			req[j+1]= temp;
+			}
+		}
+	}
+	end = req[n-1];
+	int small = req[0];
+    int total = end-head+end-small;
+	float avg= (float)total/n;
+	printf("Average Head Movement is %.2f",avg);
+	return 0;
+}
