0adba208b757f05a6916321c7381a4632820798c
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2023-11-30 10:43:59 +0530

    Update 9.inter-process_communication.c

--- 9.inter-process_communication.c
+++ 9.inter-process_communication.c
@@ -4,24 +4,19 @@
 #include <sys/types.h>
 #include <sys/msg.h>
 
-struct message {
-    long mtype;
-    char mtext[100];
-};
-
 int main() {
     // Create or get a message queue
     key_t key = ftok("message_queue_example", 65);
     int msgid = msgget(key, 0666 | IPC_CREAT);
 
     // Send a message
-    struct message msg = {1, "Hello from Process 1"};
-    msgsnd(msgid, &msg, sizeof(msg), 0);
-    printf("Message sent: %s\n", msg.mtext);
+    char msg[100] = "Hello from Process 1";
+    msgsnd(msgid, msg, sizeof(msg), 0);
+    printf("Message sent: %s\n", msg);
 
     // Receive a message
-    msgrcv(msgid, &msg, sizeof(msg), 1, 0);
-    printf("Message received: %s\n", msg.mtext);
+    msgrcv(msgid, msg, sizeof(msg), 1, 0);
+    printf("Message received: %s\n", msg);
 
     // Remove the message queue
     msgctl(msgid, IPC_RMID, NULL);
