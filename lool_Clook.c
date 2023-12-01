ef36ef0e7f83170befbd943ed440f20edc702dd5
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2023-12-01 06:26:20 +0530

    Update lool_Clook.c

--- lool_Clook.c
+++ lool_Clook.c
@@ -33,3 +33,56 @@ int main()
 	printf("Average Head Movement is %.2f",avg);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("enter the number of requests: ");
+	scanf("%d",&n);
+	int req[n+1];
+	printf("enter the requests: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&req[i]);
+	}
+	int head;
+	printf("enter the initial head position: ");
+	scanf("%d",&head);
+	req[n]=head;
+    int end;
+    for(i=0;i<n;i++)
+    {
+    	for(j=0;j<n-i;j++)
+    	{
+    		if(req[j]>req[j+1])
+    		{
+    			int temp = req[j];
+    			req[j]=req[j+1];
+    			req[j+1]= temp;
+			}
+		}
+	}
+	int key;
+	for(i=0;i<=n;i++)
+	{
+		if(req[i]==head)
+		{
+			key = i;
+		}
+	}
+	end = req[n];
+	int small = req[0];
+    int total = end-head+end-0+req[key-1]-0;
+	float avg= (float)total/n;
+	printf("Average Head Movement is %.2f",avg);
+	return 0;
+}
